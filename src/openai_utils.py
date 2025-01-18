7d974b7df1cbd89e39a69f14d284d730b040b257
Jack Wiebe <jack.wiebe@mail.utoronto.ca>
2025-01-18 14:59:07 -0500

    update openai

--- src/openai_utils.py
+++ src/openai_utils.py
@@ -3,19 +3,24 @@
 
 from config import get_api_key
 from openai import OpenAI
+from pathlib import Path
 import logging
 
 # Initialize client
 openai_key = get_api_key('OPENAI_API_KEY')
 client = OpenAI(api_key=openai_key)
 
-def translate_to_language(language, text):
+
+def translate_to_language(text, language):
     try:
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a translator. Your job is to directly translate as accurately as " +
-                 "possible and say nothing else"},
+                {
+                    "role": "system", 
+                    "content": "You are a translator. Your job is to directly translate input phrases as culturally " +
+                               "accurate as possible and say nothing else"
+                },
                 {
                     "role": "user",
                     "content": f"Translate the following text into {language}: {text}"
@@ -27,12 +32,17 @@ def translate_to_language(language, text):
         logging.error(f"Error during translation to {language}: {e}")
         return None
 
-def generate_response(text):
+
+def generate_response(text, emotion):
     try:
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a toy, and a child is speaking to you."},
+                {
+                    "role": "system", 
+                    "content": "You are a toy, and a child is speaking to you. Based on their demeanor, they appear to be feeling " +
+                    f"{emotion}. If this emotion is neutral, ignore it when generating a response."
+                },
                 {
                     "role": "user",
                     "content": f"Generate an appropriate, brief response to this comment: {text}"
@@ -44,5 +54,17 @@ def generate_response(text):
         logging.error(f"Error during response generation: {e}")
         return None
 
-def translate_response_to_language(language, response):
-    return translate_to_language(language, response)
+
+def translate_response_to_language(response, language):
+    return translate_to_language(response, language)
+
+
+def create_text_to_speech(text, filename):
+    speech_filepath = Path(__file__).parent / f"{filename}"
+
+    with client.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="sage",
+        input=f"{text}",
+    ) as response:
+        response.stream_to_file(speech_filepath)
